85cc5a9a9ddc12197e8f93639b3f79d63338363e
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2018-10-07 11:10:16 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function printBadges(names){
+  for (let i = 0; i < names.length; i ++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+  }
+    return names;
+}
+
+// printBadges(['Ada', 'Brendan', 'Ali']);
+
+function tailsNeverFails() {
+  let toss = Math.random();
+  let count = 0
+  while (toss >= 0.5) {
+    toss = Math.random();
+    count ++;
+  }
+  return `You got ${count} tails in a row!`
+}
